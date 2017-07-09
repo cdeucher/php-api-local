02a27aba56060246d20c6e68885fe0d0a4e9c77f
ryanface <ryanface_metin2@yahoo.com.br>
2017-07-08 19:38:04 -0700

    add

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 $dbhost = $OPENSHIFT_MYSQL_DB_HOST;
+
 $dbport = $OPENSHIFT_MYSQL_DB_PORT;
+
 $dbuser = "adminmtGQWWG";
+
 $dbpwd = "AAWHI28BByHP";
+
 $dbname = "php";
 $connection = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
 if (!$connection) {
@@ -11,10 +15,10 @@ if (!$connection) {
   echo "Connected to database.<br>";
 }
 $dbconnection = mysql_select_db($dbname);
-$query = "SELECT * from users";
+$query = "SELECT * from cad";
 $rs = mysql_query($query);
 while ($row = mysql_fetch_assoc($rs)) {
-  echo $row['user_id'] . " " . $row['username'] . "\n";
+  echo $row['id'] . "\n";
 }
 mysql_close();
 ?>
